f2c90a757f62ff48d0db6a833b5f388eae8b4464
Kemeow815 <kemiao@kmblog.icu>
2025-05-11 17:35:42 +0800

    update staticPage content for new language

--- src/app/static-page.ts
+++ src/app/static-page.ts
@@ -1,4 +1,11 @@
 export interface StaticPageParams extends Record<string, string | string[]> {
   tab: string
 }
-export const staticPage = ['首页', '文章', '标签', '项目', '关于', '设置']
+export const staticPage = [
+  'All',
+  'Posts',
+  'Tags',
+  'Projects',
+  'About',
+  'Setting',
+]
