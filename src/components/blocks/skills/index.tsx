2eb5e1cb3072b6c82a48908e368aefae110eef1a
Kemeow815 <kemiao@kmblog.icu>
2025-05-11 17:16:11 +0800

    add telegram icon and update Skills text

--- src/components/blocks/skills/index.tsx
+++ src/components/blocks/skills/index.tsx
@@ -15,6 +15,7 @@ import reactSvg from '@/images/logo/react.svg'
 import reactNativeSvg from '@/images/logo/react-native.svg'
 import swiftSvg from '@/images/logo/swift.svg'
 import tailwindCSSSvg from '@/images/logo/tailwind-css.svg'
+import telegramSvg from '@/images/logo/telegram.svg'
 import typeScriptSvg from '@/images/logo/typescript.svg'
 import vueSvg from '@/images/logo/vue.svg'
 import webpackSvg from '@/images/logo/webpack.svg'
@@ -40,6 +41,7 @@ const images: StaticImageData[] = [
   nestjsSvg,
   prismaSvg,
   graphqlSvg,
+  telegramSvg,
 ]
 export const Skills = () => {
   return (
@@ -49,7 +51,7 @@ export const Skills = () => {
     >
       <div className='absolute right-0 top-0 w-20 -translate-y-1/2 translate-x-1/4 rotate-12 scale-75 rounded-lg p-1 shadow-lg before:absolute before:inset-x-0 before:bottom-0 before:z-20 before:origin-bottom before:scale-y-50 before:rounded-lg before:border-[40px] before:border-transparent before:border-b-red-500 before:border-l-red-500 before:content-["_"] after:absolute after:inset-x-0 after:bottom-0 after:z-10 after:origin-bottom after:scale-y-50 after:rounded-lg after:border-[40px] after:border-transparent after:border-b-red-500 after:border-r-red-500 after:brightness-75 after:content-["_"] dark:before:border-b-blue-950 dark:before:border-l-blue-950 dark:after:border-b-blue-950 dark:after:border-r-blue-950 xl:scale-100'>
         <span className='block h-10 -translate-y-4 rounded bg-amber-50 px-2 py-1 uppercase text-slate-800 shadow dark:bg-gray-200'>
-          Skills
+          图标
         </span>
       </div>
       <Galton images={images} />
